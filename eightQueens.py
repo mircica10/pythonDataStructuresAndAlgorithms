3369ddf23083e927b5304de115b37a05092275a1
baby A <mircica10@yahoo.com>
2019-01-25 21:54:27 +0100

    queens

---
+++ eightQueens.py
@@ -0,0 +1,31 @@
+def queensHelper():
+  solution = []
+  queens(0, solution)
+
+def queens(i, solution):
+  if( len(solution) == 8):
+    printSolution(solution)
+  else:
+    for x in range(i, 8):
+      for y in range(0, 8):
+        isValid = valid(x,y,solution)
+        if(isValid):
+          solution.append((x,y))
+          queens(x, solution)
+          solution.pop()
+
+def printSolution(solution):
+  print("solution")
+  for i in range(0, len(solution)):
+    point = solution[i]
+    print(f'x:{point[0]} y:{point[1]}')
+  print("end solution")
+
+def valid(x, y, solution):
+  for i in range(0, len(solution)):
+    currentQueen = solution[i]
+    if  (currentQueen[0] - x) == 0 or (currentQueen[1] - y) == 0 or ( abs(currentQueen[1] - y) == abs(currentQueen[0] - x) ) :
+      return False
+  return True
+
+queensHelper()
